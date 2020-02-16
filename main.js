f3987f9f8d1ac276cea330e81d97bb2dfea76c3d
wxyzcctv <2846629226@qq.com>
2020-02-16 20:24:35 +0800

    添加铅笔和橡皮擦图标和颜色

--- main.js
+++ main.js
@@ -10,23 +10,70 @@ listenToUser(yyy)
 var eraserEable = false
 eraser.onclick = function(){
     eraserEable = !eraserEable
-    actions.className = "actions x"
+    eraser.classList.add("active")
+    pen.classList.remove("active")
 }
-bush.onclick = function(){
+pen.onclick = function(){
     eraserEable = !eraserEable
-    actions.className = "actions"
+    pen.classList.add("active")
+    eraser.classList.remove("active")
+}
+black.onclick = function(){
+    context.fillStyle = "black"
+    context.strokeStyle = "black"
+    black.classList.add("active")
+    red.classList.remove("active")
+    green.classList.remove("active")
+    blue.classList.remove("active")
+    yellow.classList.remove("active")
+}
+red.onclick = function(){
+    context.fillStyle = "red"
+    context.strokeStyle = "red"
+    black.classList.remove("active")
+    red.classList.add("active")
+    green.classList.remove("active")
+    blue.classList.remove("active")
+    yellow.classList.remove("active")
+}
+green.onclick = function(){
+    context.fillStyle = "green"
+    context.strokeStyle = "green"
+    black.classList.remove("active")
+    red.classList.remove("active")
+    green.classList.add("active")
+    blue.classList.remove("active")
+    yellow.classList.remove("active")
+}
+blue.onclick = function(){
+    context.fillStyle = "blue"
+    context.strokeStyle = "blue"
+    black.classList.remove("active")
+    red.classList.remove("active")
+    green.classList.remove("active")
+    blue.classList.add("active")
+    yellow.classList.remove("active")
+}
+yellow.onclick = function(){
+    context.fillStyle = "yellow"
+    context.strokeStyle = "yellow"
+    black.classList.remove("active")
+    red.classList.remove("active")
+    green.classList.remove("active")
+    blue.classList.remove("active")
+    yellow.classList.add("active")
 }
 
 /********/
 function drawCricle(x,y,radius){
     context.beginPath();
-    context.strokeStyle = 'black'
+    // context.strokeStyle = 'black'
     context.arc(x,y,radius,0,Math.PI*2)
     context.fill()
 }
 function drawLine(x1,y1,x2,y2){
     context.beginPath();
-    context.strokeStyle = 'black'
+    // context.strokeStyle = 'black'
     context.moveTo(x1,y1)
     context.lineWidth = 5
     context.lineTo(x2,y2)

84e1c5c6ca690812c6d0d53fad8181e00ebc8f3a
wxyzcctv <2846629226@qq.com>
2020-02-16 12:10:41 +0800

    优化代码并添加橡皮擦和画笔的按钮

--- main.js
+++ main.js
@@ -1,49 +1,23 @@
 var yyy = document.getElementById('xxx');
-
-setCanvasSize()
-
-window.onresize = function(){
-    this.setCanvasSize()
-}
-
-function setCanvasSize(){
-    var pageWidth = document.documentElement.clientWidth
-    var pageHeight = document.documentElement.clientHeight
-
-    yyy.width = pageWidth
-    yyy.height =pageHeight
-}
 var context = yyy.getContext('2d')
 var using = false
 var lastPoint = {'x':undefined,'y':undefined}
-yyy.onmousedown = function(a){
-    var x = a.clientX;
-    var y = a.clientY;
-    using = true
-    if(eraserEable){
-        clearEabel(x,y)
-    }else{
-        lastPoint = {'x':x,'y':y}
-        drawCricle(x,y,2)
-    }
+
+autoSetCanvasSize(yyy)
+
+listenToMouse(yyy)
+
+var eraserEable = false
+eraser.onclick = function(){
+    eraserEable = !eraserEable
+    actions.className = "actions x"
 }
-yyy.onmousemove = function(a){
-    var x = a.clientX;
-    var y = a.clientY;
-    var newPoint = {'x':x,'y':y};
-    if(using){
-        if(eraserEable){
-            clearEabel(lastPoint.x,lastPoint.y)
-        }else{
-            drawCricle(x,y,2)
-            drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
-        }
-    }
-    lastPoint = newPoint
+bush.onclick = function(){
+    eraserEable = !eraserEable
+    actions.className = "actions"
 }
-yyy.onmouseup = function(a){
-    using = false
-}
+
+/********/
 function drawCricle(x,y,radius){
     context.beginPath();
     context.strokeStyle = 'black'
@@ -59,10 +33,51 @@ function drawLine(x1,y1,x2,y2){
     context.stroke()
     context.closePath()
 }
-var eraserEable = false
-eraser.onclick = function(){
-    eraserEable = !eraserEable
-}
 function clearEabel(x,y){
     context.clearRect(x-5,y-5,10,10)
+}
+function autoSetCanvasSize(canvas){
+    setCanvasSize()
+
+    window.onresize = function(){
+        setCanvasSize()
+    }
+    
+    function setCanvasSize(){
+        var pageWidth = document.documentElement.clientWidth
+        var pageHeight = document.documentElement.clientHeight
+    
+        canvas.width = pageWidth
+        canvas.height =pageHeight
+    }
+}
+function listenToMouse(canvas){
+    canvas.onmousedown = function(a){
+        var x = a.clientX;
+        var y = a.clientY;
+        using = true
+        if(eraserEable){
+            clearEabel(x,y)
+        }else{
+            lastPoint = {'x':x,'y':y}
+            drawCricle(x,y,2)
+        }
+    }
+    canvas.onmousemove = function(a){
+        var x = a.clientX;
+        var y = a.clientY;
+        var newPoint = {'x':x,'y':y};
+        if(using){
+            if(eraserEable){
+                clearEabel(lastPoint.x,lastPoint.y)
+            }else{
+                drawCricle(x,y,2)
+                drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+            }
+        }
+        lastPoint = newPoint
+    }
+    canvas.onmouseup = function(a){
+        using = false
+    }
 }
